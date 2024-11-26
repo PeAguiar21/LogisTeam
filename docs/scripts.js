0b64ac4c2ed696c3935bf96713eea7f1e594a375
PeAguiar21 <pedro-henriqueaguiar@hotmail.com>
2024-11-25 22:02:36 -0300

    realizando implementacao com o cadastro de fornecedores

--- docs/scripts.js
+++ docs/scripts.js
@@ -3,25 +3,29 @@ document.addEventListener('DOMContentLoaded', function () {
                  document.getElementById('fornecedor-form') || 
                  document.getElementById('estoque-form');
 
+
+    let apiUrl = ''
+    let getRegistro = ''
+
     const deleteButton = document.querySelector('.delete-btn');
 
     if (form) {
+        if (form.id === 'produto-form') {
+            apiUrl = 'http://localhost:3322/produtos';
+            getRegistro = 'getProduto'
+        } else if (form.id === 'fornecedor-form') {
+            apiUrl = 'http://localhost:3322/fornecedores';
+            getRegistro = 'getFornecedor'
+        } else if (form.id === 'estoque-form') {
+            apiUrl = 'http://localhost:3322/inventario';
+            getRegistro = 'getEstoque'
+        }
         form.addEventListener('submit', async function (event) {
             event.preventDefault();
 
             const formData = new FormData(event.target);
-
-            const data = {
-                id: document.getElementById('id').value,
-                nome: formData.get('nome'),
-                codigo: formData.get('codigo'),
-                preco: parseFloat(formData.get('preco')),
-                categoria: formData.get('categoria'),
-                descricao: formData.get('descricao') || '',
-                quantidade: parseInt(formData.get('quantidade')) || 0,
-            };
-        
-
+            
+            let data = {}
             let targetUrl = '';
             let method = 'POST';
 
@@ -32,8 +36,29 @@ document.addEventListener('DOMContentLoaded', function () {
                 } else {
                     targetUrl = 'http://localhost:3322/produtos';
                 }
+                data = {
+                    id: document.getElementById('id').value,
+                    nome: formData.get('nome'),
+                    codigo: formData.get('codigo'),
+                    preco: parseFloat(formData.get('preco')),
+                    categoria: formData.get('categoria'),
+                    descricao: formData.get('descricao') || '',
+                    quantidade: parseInt(formData.get('quantidade')) || 0,
+                };
             } else if (form.id === 'fornecedor-form') {
-                targetUrl = 'http://localhost:3322/fornecedores';
+                if (!isNewRegister) {
+                    targetUrl = `http://localhost:3322/fornecedores/${codeInput.value}`;
+                    method = 'PUT';
+                } else {
+                    targetUrl = 'http://localhost:3322/fornecedores';
+                }
+                data = {
+                    id: document.getElementById('id').value,
+                    nome: formData.get('nome'),
+                    cpf_cnpj: formData.get('cpfcnpj'),
+                    dataCadastro: document.getElementById('data-cadastro').value,
+                    contato: formData.get('contato')
+                };
             } else if (form.id === 'estoque-form') {
                 targetUrl = 'http://localhost:3322/inventario';
             }
@@ -50,8 +75,6 @@ document.addEventListener('DOMContentLoaded', function () {
                 if (!response.ok) {
                     throw new Error(`Erro: ${response.statusText}`);
                 }
-
-                const result = await response.json();
                 Toastify({
                     text: "Registro salvo com sucesso",
                     duration: 3000,
@@ -158,13 +181,12 @@ document.addEventListener('DOMContentLoaded', function () {
 
     const codeInput = document.querySelector('.code-page');
     const nameInput = document.getElementById('nome');
-    const formFields = document.querySelectorAll('#produto-form input, #produto-form select, #produto-form textarea');
-    const apiUrl = 'http://localhost:3322/produtos';
+    const formFields = document.querySelectorAll('#produto-form input, #produto-form select, #produto-form textarea,#fornecedor-form input, #fornecedor-form select, #fornecedor-form textarea ');
 
     let isNewRegister = false
 
     formFields.forEach(field => {
-        if (!field.classList.contains('field-disponivel')) { 
+        if (!field.classList.contains('field-disponivel') || !field.classList.contains('data-cadastro')) { 
             field.disabled = true;
         }
     });
@@ -189,16 +211,31 @@ document.addEventListener('DOMContentLoaded', function () {
         document.getElementById('descricao').value = '';
     }
 
-    async function checkProductExists(id) {
+    function preencherCamposComFornecedor(fornecedor) {
+        if (fornecedor) {
+            document.getElementById('nome').value = fornecedor.nome || '';
+            document.getElementById('cpfcnpj').value = fornecedor.cpf_cnpj || '';
+            document.getElementById('data-cadastro').value = fornecedor.dataCadastro;
+            document.getElementById('contato').value = fornecedor.contato || '';
+        }
+    }
+
+    function limparCamposFornecedores() {
+        document.getElementById('nome').value =  '';
+        document.getElementById('cpfcnpj').value =  '';
+        document.getElementById('contato').value = '';
+    }
+
+    async function checkRegistro(id) {
         try {
-            const response = await fetch(`${apiUrl}/getProduto/${id}`);
+            const response = await fetch(`${apiUrl}/${getRegistro}/${id}`);
             if (response.ok) {
                 const data = await response.json();
                 return data;
             } else if (response.status === 404) {
                 return null;
             } else {
-                throw new Error(`Erro ao verificar o produto: ${response.statusText}`);
+                throw new Error(`Erro ao verificar o registro: ${response.statusText}`);
             }
         } catch (error) {
             console.error('Erro ao conectar-se à API:', error);
@@ -206,40 +243,86 @@ document.addEventListener('DOMContentLoaded', function () {
         }
     }
 
-    codeInput.addEventListener('blur', async () => {
-        const id = codeInput.value.trim();
-        if (!id) {
-            return;
-        }
+    codeInput?.addEventListener('blur', async () => {
+        const id = Number(codeInput.value.trim());
 
-        const produto = await checkProductExists(id);
-        if (produto) {
-            formFields.forEach(field => {
-                if (!field.classList.contains('field-disponivel')) {
-                    field.disabled = false;
-                }
-            });
-            preencherCamposComProduto(produto);
-            isNewRegister = false
-        } else {
+        if (!id) {
             Toastify({
-                text: "Produto não encontrado, clicar no botão Novo!",
+                text: "Por favor, insira um valor válido!",
                 duration: 3000,
                 close: true,
                 gravity: "top",
                 position: "right",
                 style: {
                     background: "linear-gradient(to right, #ba181b, #e5383b",
-                  },
+                },
                 stopOnFocus: true
             }).showToast();
-            formFields.forEach(field => {
-                if (!field.classList.contains('field-disponivel')) {
-                    field.disabled = true;
-                }
-            });
             codeInput.value = '';
-            limparCamposProdutos()
+            return;
+        }
+        if (getRegistro === 'getProduto') {
+            const produto = await checkRegistro(id);
+            if (produto) {
+                formFields.forEach(field => {
+                    if (!field.classList.contains('field-disponivel')) {
+                        field.disabled = false;
+                    }
+                });
+                preencherCamposComProduto(produto);
+                isNewRegister = false
+            } else {
+                Toastify({
+                    text: "Produto não encontrado, clicar no botão Novo!",
+                    duration: 3000,
+                    close: true,
+                    gravity: "top",
+                    position: "right",
+                    style: {
+                        background: "linear-gradient(to right, #ba181b, #e5383b",
+                      },
+                    stopOnFocus: true
+                }).showToast();
+                formFields.forEach(field => {
+                    if (!field.classList.contains('field-disponivel')) {
+                        field.disabled = true;
+                    }
+                });
+                codeInput.value = '';
+                limparCamposProdutos()
+            }
+        } else if (getRegistro === 'getFornecedor') {
+            const Fornecedor = await checkRegistro(id);
+            if (Fornecedor) {
+                formFields.forEach(field => {
+                    if (!field.classList.contains('data-cadastro')) {
+                        field.disabled = false;
+                    }
+                });
+                preencherCamposComFornecedor(Fornecedor);
+                isNewRegister = false
+            } else {
+                Toastify({
+                    text: "Fornecedor não encontrado, clicar no botão Novo!",
+                    duration: 3000,
+                    close: true,
+                    gravity: "top",
+                    position: "right",
+                    style: {
+                        background: "linear-gradient(to right, #ba181b, #e5383b",
+                      },
+                    stopOnFocus: true
+                }).showToast();
+                formFields.forEach(field => {
+                    if (!field.classList.contains('data-cadastro')) {
+                        field.disabled = true;
+                    }
+                });
+                codeInput.value = '';
+                limparCamposFornecedores()
+            }
+        } else {
+
         }
     });
 
@@ -248,7 +331,7 @@ document.addEventListener('DOMContentLoaded', function () {
     if (newButton) {
         newButton.addEventListener('click', async () => {
             try {
-                const response = await fetch(`${apiUrl}/getProdutoUltimo`);
+                const response = await fetch(`${apiUrl}/getUltimoRegistro`);
                 if (!response.ok) {
                     throw new Error(`Erro ao buscar o último código: ${response.statusText}`);
                 }
@@ -261,20 +344,100 @@ document.addEventListener('DOMContentLoaded', function () {
                         nameInput.focus();
                     }
 
-                    
-                    formFields.forEach(field => {
-                        if (!field.classList.contains('field-disponivel')) {
-                            field.disabled = false;
-                        }
-                    });
 
                     isNewRegister = true
 
-                    limparCamposProdutos()
+                    if (getRegistro === 'getProduto') {
+                        limparCamposProdutos()
+                        formFields.forEach(field => {
+                            if (!field.classList.contains('field-disponivel')) {
+                                field.disabled = false;
+                            }
+                        });
+                    } else if (getRegistro === 'getFornecedor') {
+                        limparCamposFornecedores()
+                        formFields.forEach(field => {
+                            if (!field.classList.contains('data-cadastro')) {
+                                field.disabled = false;
+                            }
+                        });
+                    } else if (getRegistro === 'getEstoque') {
+
+                    }
+                    
                 }
             } catch (error) {
                 console.error('Erro ao buscar o último código:', error);
             }
         });
+    }
+
+    const productList = document.querySelector('.product-list');
+    const supplierList = document.querySelector('.supplier-list');
+
+    const fetchProducts = async () => {
+        try {
+            const response = await fetch('http://localhost:3322/produtos');
+            if (!response.ok) {
+                throw new Error('Erro ao buscar produtos.');
+            }
+            const products = await response.json();
+            productList.innerHTML = '';
+
+            if (products.length === 0) {
+                const noRecordsMessage = document.createElement('li');
+                noRecordsMessage.textContent = 'Nenhum produto cadastrado.';
+                noRecordsMessage.style.fontStyle = 'italic';
+                noRecordsMessage.style.color = 'gray';
+                productList.appendChild(noRecordsMessage);
+            } else {
+                products.forEach(product => {
+                    const listItem = document.createElement('li');
+                    listItem.innerHTML = `
+                        <img class="img-home" src="./images/Product.svg" alt="">
+                        ${product.id}: ${product.nome} - ${product.quantidade}
+                    `;
+                    productList.appendChild(listItem);
+                });
+            }
+        } catch (error) {
+            console.error('Erro:', error.message);
+        }
+    };
+
+    const fetchFornecedores = async () => {
+        try {
+            const response = await fetch('http://localhost:3322/fornecedores');
+            if (!response.ok) {
+                throw new Error('Erro ao buscar fornecedores.');
+            }
+            const suppliers = await response.json();
+
+            supplierList.innerHTML = '';
+
+            if (suppliers.length === 0) {
+                const noRecordsMessage = document.createElement('li');
+                noRecordsMessage.textContent = 'Nenhum fornecedor cadastrado.';
+                noRecordsMessage.style.fontStyle = 'italic';
+                noRecordsMessage.style.color = 'gray';
+                supplierList.appendChild(noRecordsMessage);
+            } else {
+                suppliers.forEach(supplier => {
+                    const listItem = document.createElement('li');
+                    listItem.innerHTML = `
+                        <img class="img-home" src="./images/Supplier.svg" alt="">
+                        ${supplier.id}: ${supplier.nome}
+                    `;
+                    supplierList.appendChild(listItem);
+                });
+            }
+        } catch (error) {
+            console.error('Erro:', error.message);
+        }
+    };
+
+    if (!getRegistro) {
+        fetchProducts();
+        fetchFornecedores();
     }
 });
